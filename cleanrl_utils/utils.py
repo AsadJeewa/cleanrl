1bb59e984b5ca3c55462a30d8469b7e8988316cd
AsadJeewa <asad.jeewa@gmail.com>
2025-12-24 10:18:27 +0200

    feat: make env utils general

--- cleanrl_utils/utils.py
+++ cleanrl_utils/utils.py
@@ -9,7 +9,7 @@ def get_base_env(env):
         env = env.env
     return env
 
-def make_env(env_id, idx, capture_video, run_name, single_reward=False, difficulty=""):
+def make_env(env_id, idx=None, seed=None, difficulty="", capture_video=False, run_name="", render=False):
     def thunk():
         # if capture_video and idx == 0:
         #     env = gym.make(env_id, render_mode="rgb_array")
@@ -20,13 +20,17 @@ def make_env(env_id, idx, capture_video, run_name, single_reward=False, difficul
         extra_kwargs = {}
         if difficulty:
             extra_kwargs["difficulty"] = DIFFICULTY[difficulty.upper()]
-        env = mo_gym.make(env_id, **extra_kwargs)
+        if render:
+            env = mo_gym.make(env_id, render_mode="human", **extra_kwargs)
+        else: 
+            env = mo_gym.make(env_id, **extra_kwargs)
         # env = mo_gym.wrappers.LinearReward(env, weight=np.array([0.8, 0.2]))
         # env = TimeLimit(env, max_episode_steps=100)  # ensure episodes end
         env = MORecordEpisodeStatistics(env, gamma=0.98)
-        if single_reward:
+        if idx is not None:
             env = SingleRewardWrapper(env, idx)
-
+        if seed is not None:
+            env.reset(seed=seed)
         return env
 
     return thunk
